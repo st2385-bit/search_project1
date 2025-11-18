80b92e77205f3862c28d2d0bc008270f1cf725e2
Latte2378 <st2378@roong-aroon.ac.th>
2025-11-18 10:18:26 +0700

    Add files via upload

--- search.php
+++ search.php
@@ -1,12 +1,12 @@
 <?php 
-$serverscript = 'server.php';// เขียนชื่อให้ตรงกับไฟล์ server 
- include($serverscript); ?>
+$serverscript = 'server.php';   // เขียนชื่อให้ตรงกับไฟล์ server 
+include($serverscript); ?>
 <!DOCTYPE html>
 <html lang="th">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="stylesheet" href="design.css"> -->
+    <link rel="stylesheet" href="design.css">
     <title>Student information</title>
 </head>
 <body>
@@ -14,32 +14,32 @@ $serverscript = 'server.php';// เขียนชื่อให้ตรงก
         <div class="result_table">
 
             <p>
-                <b>ID:</b> <?= htmlspecialchars($nid) ?><form method="post" action="search.php" style="display:inline-block; margin-left:20px;">
+                <form method="post" action="delete.php" class="delete">
+                    <b>ID : </b> <?= htmlspecialchars($nid) ?>
+                    <button type="submit" class="delete">delete</button>
+                </form>
+                <form method="post" action="search.php" style="display:inline-block; margin-left:20px;">
                     <input type="number" name="st" placeholder="new search" class="update" required>
                     <button type="submit" class="submit_button">ค้นหา</button>
                 </form>
             </p><br>
 
-            <form method="post" action="search.php" class="update_form">
+            <form method="post" action="update.php" class="update_form">
                 <input type="hidden" name="student_id" value="<?= $student['student_id'] ?>"> <!--ขาดไม่ได้เดี๋ยวโปรแกรมเอ๋อ-->
 
-                <label>ชื่อ:</label><label><?= htmlspecialchars($nfirstname) ?></label>
-                <input type="text" name="nameupdate" class="update"><br><br>
+                <label>ชื่อ : </label><label><?= htmlspecialchars($nfirstname) ?></label>
+                <button type="submit" class="update">edit</button><br><br>
 
-                <label>นามสกุล:</label><label><?= htmlspecialchars($nlastname) ?></label>
-                <input type="text" name="lastname" class="update"><br><br>
+                <label>นามสกุล : </label><label><?= htmlspecialchars($nlastname) ?></label>
+                <button type="submit" class="update">edit</button><br><br>
 
-                <label>ชื่อเล่น:</label><label><?= htmlspecialchars($nnickname) ?></label>
-                <input type="text" name="nickname" class="update"><br><br>
+                <label>ชื่อเล่น : </label><label><?= htmlspecialchars($nnickname) ?></label>
+                <button type="submit" class="update">edit</button><br><br>
 
-                <label>ชั้น:</label><label><?= htmlspecialchars($nclass) ?></label>
-                <input type="text" name="class" class="update"><br><br>
+                <label>ชั้น : </label><label><?= htmlspecialchars($nclass) ?></label>
+                <button type="submit" class="update">edit</button><br><br>
 
-                <label>เลขที่:</label>
-                <label><?= htmlspecialchars($nnumber) ?></label>
-                <!-- <input type="number" name="number" value="<?= htmlspecialchars($nnumber) ?>" class="update"> -->
-                <br><br>
-                <button type="submit" class="name_button">save your update</button>
+                <label>เลขที่ : </label><label><?= htmlspecialchars($nnumber) ?></label><br><br>
             </form>
         </div>
 
