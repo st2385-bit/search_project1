87d272dd8db1292639cff5cc08fbbfc9a2b9b351
Latte2378 <st2378@roong-aroon.ac.th>
2025-11-24 20:15:55 +0700

    Add files via upload

--- search.php
+++ search.php
@@ -7,6 +7,7 @@ include($serverscript); ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="design.css">
+    <script src="show.js"></script>
     <title>Student information</title>
 </head>
 <body>
@@ -15,8 +16,8 @@ include($serverscript); ?>
 
             <p>
                 <form method="post" action="delete.php" class="delete">
-                    <b>ID : </b> <?= htmlspecialchars($nid) ?>
-                    <button type="submit" class="delete">delete</button>
+                    <b>st : </b> <?= htmlspecialchars($nid) ?>
+                    <button type="submit" name="delete" onclick="return confirmDelete();" class="delete">delete</button>
                 </form>
                 <form method="post" action="search.php" style="display:inline-block; margin-left:20px;">
                     <input type="number" name="st" placeholder="new search" class="update" required>
@@ -24,20 +25,24 @@ include($serverscript); ?>
                 </form>
             </p><br>
 
-            <form method="post" action="update.php" class="update_form">
+            <form method="post" action="search.php" class="update_form">
                 <input type="hidden" name="student_id" value="<?= $student['student_id'] ?>"> <!--ขาดไม่ได้เดี๋ยวโปรแกรมเอ๋อ-->
 
-                <label>ชื่อ : </label><label><?= htmlspecialchars($nfirstname) ?></label>
-                <button type="submit" class="update">edit</button><br><br>
+                <label>ชื่อจริง : </label><input type="text" id="first_name" name="first_name" value="<?= htmlspecialchars($nfirstname) ?>">
+                <button type="submit" name="first_name" onclick="return confirmEdit();" class="update">edit</button>
+                <button type="submit" name="undo_firstname" class="undo">undo</button><br><br>
 
-                <label>นามสกุล : </label><label><?= htmlspecialchars($nlastname) ?></label>
-                <button type="submit" class="update">edit</button><br><br>
+                <label>นามสกุล : </label><input type="text" id="last_name" name="last_name" value="<?= htmlspecialchars($nlastname) ?>">
+                <button type="submit" name="last_name" onclick="return confirmEdit();" class="update">edit</button>
+                <button type="submit" name="undo_lastname" class="undo">undo</button><br><br>
 
-                <label>ชื่อเล่น : </label><label><?= htmlspecialchars($nnickname) ?></label>
-                <button type="submit" class="update">edit</button><br><br>
+                <label>ชื่อเล่น : </label><input type="text" id="nickname" name="nickname" value="<?= htmlspecialchars($nnickname) ?>">
+                <button type="submit" name="nickname" onclick="return confirmEdit();" class="update">edit</button>
+                <button type="submit" name="undo_nickname" class="undo">undo</button><br><br>
 
-                <label>ชั้น : </label><label><?= htmlspecialchars($nclass) ?></label>
-                <button type="submit" class="update">edit</button><br><br>
+                <label>ชั้น : </label><input type="text" id="class" name="class" value="<?= htmlspecialchars($nclass) ?>">
+                <button type="submit" name="class" onclick="return confirmEdit();" class="update">edit</button>
+                <button type="submit" name="undo_class" class="undo">undo</button><br><br>
 
                 <label>เลขที่ : </label><label><?= htmlspecialchars($nnumber) ?></label><br><br>
             </form>
@@ -56,7 +61,7 @@ include($serverscript); ?>
     <?php endif; ?>
 
     <form action="index.html" method="get">
-        <button type="submit">Back to Home</button>
+        <button type="submit">Back to home</button>
     </form>
 </body>
 </html>
